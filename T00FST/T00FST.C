871ab5fa385f248db7eebc558d161f4d4e6dbb22
Galinsky Vitaly (VG4) <cgsg@yandex.ru>
2020-06-01 12:12:50 +0300

    First commit.

---
+++ T00FST/T00FST.C
@@ -0,0 +1,7 @@
+/* Galinsky Vitaly, 01.06.2020 */
+#include <windows.h>
+
+void main( void )
+{
+  MessageBox(NULL, "Summer practice 2020.\nCGSG Primary Group", "Info", MB_OK | MB_ICONINFORMATION);
+}
